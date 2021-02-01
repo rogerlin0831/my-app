04a97cf24ac92b53eb9bbc54d6b1511b9e28cdb0
rogerlin0831 <77921427+rogerlin0831@users.noreply.github.com>
2021-02-01 23:07:50 +0800

    v2021-02-01

---
+++ src/object/timeCount.jsx
@@ -0,0 +1,44 @@
+var callBack;
+var CountNum;
+
+const startTimeCount = (call, _inputNum) => {
+  callBack = call;
+  // 存時間.
+  console.log('tmpname is ' + _inputNum);
+  var time = Number(_inputNum);
+  CountNum = time;
+
+  console.log(time);
+  if (time && time < 60 && time > 0) {
+    setTimeout(TestCountDown, 1000);
+  } else {
+    alert('time error');
+  }
+};
+//
+
+// 倒數.tick
+// 回傳倒數時間.
+const getTimeString = () => {
+  var time = Number(CountNum);
+  if (time) {
+    var min = Math.floor(time / 60);
+    var sec = time % 60;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+    return min + ':' + sec;
+  }
+  return '00:00';
+};
+
+const TestCountDown = () => {
+  CountNum--;
+  var oPtime = CountNum;
+  if (oPtime === 0) {
+    callBack();
+    return;
+  }
+  setTimeout(TestCountDown, 1000);
+};
+
+export { getTimeString, startTimeCount };
